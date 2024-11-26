0ed319d2262b6703b545548793b621e107936615
Bolutife23 <oluwoleodunayomide@gmail.com>
2024-11-26 11:44:59 +0000

    Update config.ts
    Meta description
    Meta Title

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Oluwole Boluwatife| Digital Marketer';
+export const SITE_DESCRIPTION = 'As a highly motivated digital marketer, I specialize in creating and executing data-driven strategies that drive business growth and customer engagement.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
