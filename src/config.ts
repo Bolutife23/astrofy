771f099a4fe17545858f856de883936e22f60694
Bolutife23 <oluwoleodunayomide@gmail.com>
2024-11-26 12:53:27 +0000

    Update config.ts
    meta description

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-// Place any global data in this file.
+ // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Oluwole Boluwatife| Digital Marketer';
+export const SITE_TITLE = 'Oluwole Boluwatife| Digital Marketer Expert';
 export const SITE_DESCRIPTION = 'As a highly motivated digital marketer, I specialize in creating and executing data-driven strategies that drive business growth and customer engagement.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
